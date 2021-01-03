d2f7f7b4c7529c0f036ea3fff798196801e8f046
诺墨 <normal@normalcoder.com>
2021-01-03 14:40:30 +0800

    feat(commander): improve command parameter prompt and help

--- constants/i18n.js
+++ constants/i18n.js
@@ -1,8 +1,10 @@
 const ZH_TIP = {
     helpInfo: {
+        description: '一个用于查找/删除 node_modules 文件夹的命令行工具。',
         version: '查看版本',
         del: '查询并选择删除',
         find: '查询',
+        help: '查看帮助',
         project: '项目名',
         projectSize: '大小',
         folder: '地址',
@@ -15,9 +17,11 @@ const ZH_TIP = {
 };
 const EN_TIP = {
     helpInfo: {
+        description: 'A command line tool to find/delete the node_modules folder.',
         version: 'View Version',
         del: 'Find And Select To Delete',
         find: 'Find',
+        help: 'Display this help message',
         project: 'Project Name',
         projectSize: 'Size',
         folder: 'Path',
